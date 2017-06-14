1bf73f46f710532ee66fbba6eb928d279e0d607a
civic <tsskmail@gmail.com>
2017-06-14 14:16:24 +0900

    add threading

--- thread.py
+++ thread.py
@@ -1,15 +1,19 @@
 import threading
+import time
 
 
-def worker(key):
+
+def worker():
     for n in range(10):
-        print(key, n)
+        print("{}: {}".format(threading.current_thread().name, n))
+        time.sleep(0.5)
 
 
-th1 = threading.Thread(target=worker, args="a")
-th2 = threading.Thread(target=worker, args="b")
+th1 = threading.Thread(target=worker, name="a")
+th2 = threading.Thread(target=worker, name="b")
 
 th1.start()
+time.sleep(0.1)
 th2.start()
 
 th1.join()
