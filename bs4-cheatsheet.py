12ade89bac94374238abe6c701c4de16cde5e085
civic <tsskmail@gmail.com>
2017-12-21 19:12:59 +0900

    navigation, selectについて

--- bs4-cheatsheet.py
+++ bs4-cheatsheet.py
@@ -5,7 +5,46 @@ from bs4 import BeautifulSoup
 def main():
     #making_soup()
 
-    navigation()
+    #navigation()
+
+    select()
+
+def select():
+    """
+    css セレクタを使った検索。jqueryっぽい
+    """
+    soup = BeautifulSoup('''
+    <html>
+        <head><title>example</title></head>
+        <body>
+            <ul>
+                <li><a href="http://example.com/" class="first">example.com</a></li>
+                <li><a href="http://www.google.com/" data="123">google.com</a></li>
+            </ul>
+        </body>
+    </html>''', 'html.parser')
+
+    # find_allは全検索してリストで返す
+    li_elements = soup.select("li")
+    print(li_elements)  # [<li><a class="first" href="http://example.com/">example.com</a></li>,
+                        #  <li><a href="http://www.google.com/">google.com</a></li>]
+
+    # select_oneは検索して最初に見つかった1つ目
+    first_li_element = soup.select_one('li')
+    print(first_li_element) # <li><a class="first" href="http://example.com/">example.com</a></li>
+    # 見つからない場合はNone
+    nothing_image_element = soup.select_one('img')
+    print(nothing_image_element)    # None
+
+    #属性検索
+    anchors_to_google = soup.select('a[href="http://www.google.com/"]')
+    print(anchors_to_google)    # [<a href="http://www.google.com/">google.com</a>]
+    #data属性存在
+    print(soup.select('a[data]'))  # [<a data="123" href="http://www.google.com/">google.com</a>]
+
+    # class指定
+    first_link_element = soup.select_one("a.first")
+    print(first_link_element)   # <a class="first" href="http://example.com/">example.com</a>
 
 def navigation():
     """
@@ -24,30 +63,41 @@ def navigation():
     </html>''', 'html.parser')
 
     # 子要素をたぐる
-    print(soup.head)
-    print(soup.head.title)  # headの下のtitle
+    print(soup.head)        # <head><title>example</title></head>
+    # headの下のtitle
+    print(soup.head.title)  # <title>example</title>
 
     # find_allは全検索してリストで返す
     li_elements = soup.find_all("li")
-    print(li_elements)
+    print(li_elements)  # [<li><a class="first" href="http://example.com/">example.com</a></li>,
+                        #  <li><a href="http://www.google.com/">google.com</a></li>]
+    # 見つからない場合は空のリスト
+    nothing_images = soup.find_all('img')
+    print(nothing_images)   # []
+
     # findは検索して最初に見つかった1つ目
     first_li_element = soup.find('li')
-    print(first_li_element)
+    print(first_li_element) # <li><a class="first" href="http://example.com/">example.com</a></li>
+    # 見つからない場合はNone
+    nothing_image_element = soup.find('img')
+    print(nothing_image_element)    # None
 
     #属性も検索条件につける
     anchors_to_google = soup.find_all("a", href="http://www.google.com/")
-    print(anchors_to_google)
+    print(anchors_to_google)    # [<a href="http://www.google.com/">google.com</a>]
 
     # classは予約後なのでアンダースコア付加
     first_link_element = soup.find("a", class_="first")
-    print(first_link_element)
+    print(first_link_element)   # <a class="first" href="http://example.com/">example.com</a>
 
-    print(first_link_element['href'])   # 属性取得
-    print(first_link_element.string)    # テキスト要素
+    # 属性取得
+    print(first_link_element['href'])   # http://example.com/
+    # テキスト要素
+    print(first_link_element.string)    # example.com
 
-    # stringとcontentsの違い
-    print(soup.body.string) #直下のテキスト要素
-    print(soup.body.contents)   #配下のすべてのsoupオブジェクト
+    # 親要素
+    parent = first_link_element.parent
+    print(parent)   # <li><a class="first" href="http://example.com/">example.com</a></li>
 
 def making_soup():
     """
